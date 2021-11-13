c50371853a8a8a4a00764441acd3dc199a5526b7
jepkess <biivincent1997@gmail.com>
2021-11-14 01:05:22 +0300

    configuring the upload_photos_dest

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
