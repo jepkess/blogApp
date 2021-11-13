f0c18c97e57b930bc46fc50818d0de012fbd1882
jepkess <biivincent1997@gmail.com>
2021-11-14 01:10:36 +0300

    create a register view faunction that renders the register template file

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,18 +23,17 @@ def login(): # login view function that renders the template file(login.html)
 
 
 
-
 @auth.route('/register', methods=['GET','POST'])
 def register(): #register view function that renders the register template file.
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user= User(email=form.email.data,username=form.username.data,password=form.password.data)
+    registration_form = RegistrationForm()
+    if registration_form.validate_on_submit():
+        user= User(email=registration_form.email.data,username=registration_form.username.data,password=registration_form.password.data)
         db.session.add(user)
         db.session.commit()
 
         mail_message("Welcome to iBlog","/welcome_user",user.email,user=user)
         return  redirect(url_for('auth.login'))
-    return render_template('register.html',registrationform=form)
+    return render_template('auth/register.html',registration_form=registration_form)
 
 
 
