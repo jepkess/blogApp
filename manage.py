856e2b8a92a2b1b9bfcf765fdfcd3ee2d4a5f787
jepkess <biivincent1997@gmail.com>
2021-11-15 19:41:12 +0300

    create the start.sh file

--- manage.py
+++ manage.py
@@ -26,5 +26,6 @@ def make_shell_context():
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
+
 if __name__ == '__main__':
     manager.run()
--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ SECRET_KEY='DEVIED'
 export MAIL_USERNAME='vincent.bii@student.moringaschool.com'
 export MAIL_PASSWORD='vini2016'
 
+python3.8 manage.py server
+
